0a74e0417878d987d8be3d44eab544cee48cfbe7
heropahn <herophan@icloud.com>
2024-03-02 20:48:32 +0800

    All files updated and ready for submission.

---
+++ assignment/reference.js
@@ -0,0 +1,14 @@
+/* 
+    ********************************************
+    All referencing belongs here in reference.js
+    ******************************************** 
+*/ 
+
+function printMe(){
+    console.log("Referencing from");
+    console.log("https://www.w3schools.com/css/");
+    console.log("https://www.w3schools.com/html/");
+    console.log("https://uiverse.io");
+    console.log("https://github.com/terencegaffudSU/6m-software-1.2-cli-git/blob/main/assignment.md");
+}
+printMe();
